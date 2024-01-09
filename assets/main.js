1c45b391808d66e5ee8cb6e98addfb3b8802e3bf
mas0110 <masoudmohammed13@gmail.com>
2024-01-09 20:13:10 +0100

    numeri multipli di 3 e 5

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,22 @@
-const ul = document.querySelector(`ul.list`)
+// costante elemento lista 
+const ul = document.querySelector(`ul.list`);
 
+// ciclo for lista numeri 0-100 
 for ( let i = 0; i<= 100; i++){
-    const element = `<li>${i}</li>`
-    ul.innerHTML += element
+    if(((i % 3) === 0 && (i % 5) === 0)){
+        console.log(i);
+        const element ='<li>FizzBuzz</li>'
+        ul.innerHTML += element
+    } else if ( (i % 3) === 0){
+        console.log(i);
+        const element ='<li>fizz</li>'
+        ul.innerHTML += element
+    } else if ((i % 5) === 0) {
+        console.log(i);
+        const element ='<li>buzz</li>'
+        ul.innerHTML += element
+    } else {
+        const element = `<li>${i}</li>`
+        ul.innerHTML += element
+    }
 }
-
